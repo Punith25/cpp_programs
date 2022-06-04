0ca11900baf603fb274ff7c6e4da39d274cbfab3
Punith Pinto <punithpinto@gmail.com>
2022-06-04 11:11:23 +0530

    new file added

---
+++ firstcode.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+int a, b, sum;
+
+int main()
+{
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << endl;
+    cout << "Enter another number: ";
+    cin >> b;
+    cout << endl;
+    cout << "Sum: " << a + b << endl;
+}
